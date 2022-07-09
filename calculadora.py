a4994e7a42c0c50bf80139a3813575f455ade646
Whewm0s <90000835+whewm0s@users.noreply.github.com>
2022-07-09 14:10:58 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,13 +1,13 @@
-contas = ['multiplicação, divisão, soma, subtração']
-tipo_de_conta = input('Qual conta você quer fazer? ')
-numero_user   = int(input('Digite aqui o primeiro número: '))
-numero_user2  = int(input('Digite aqui o segundo número: '))
+print("[1] Para multiplicação\n[2] Para divisão\n[3] Para soma\n[4] Para subtração")
+tipo_de_conta = int(input('Qual conta você quer fazer? '))
+n1 = int(input('Digite aqui o primeiro número: '))
+n2 = int(input('Digite aqui o segundo número: '))
 
-if 'multiplicação' in tipo_de_conta:
-    print('O resultado da conta é {}'.format(numero_user * numero_user2))
-elif 'divisão' in tipo_de_conta:
-    print('O resultado da conta é {}'.format(round(numero_user / numero_user2)))
-elif 'soma' in tipo_de_conta:
-    print('O resultado da conta é {}'.format(numero_user + numero_user2))
-else:
-    print('O resultado da conta é {}'.format(numero_user - numero_user2))
+if tipo_de_conta == 1:
+    print('O resultado da conta é {}'.format(n1 * n2))
+elif tipo_de_conta == 2:
+    print('O resultado da conta é {}'.format(n1/n2))
+elif tipo_de_conta == 3:
+    print('O resultado da conta é {}'.format(n1 + n2))
+elif tipo_de_conta == 4:
+    print('O resultado da conta é {}'.format(n1 - n2))
